ad4866c55fcb3b5b3e62287263ddec363afe226c
Bradly Lewis <bradlylewisdev@gmail.com>
2021-06-14 19:09:34 -0400

    update index.js task 3

--- index.js
+++ index.js
@@ -84,18 +84,38 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningCB, number){
+  let homeScore = 0;
+  let awayScore = 0;
+
+  for(let i = 0; i < number; i++){
+    // const currScore = inningCB(number);
+    homeScore = homeScore + inningCB(number);
+    awayScore = awayScore + inningCB(number);
+    
+    console.log(`Inning ${i}. Home: ${homeScore} Away: ${awayScore}`)
+  }
+  return {
+    Home: homeScore,
+    Away: awayScore,
+  }
 }
+
+console.log(finalScore(inning, 5))
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCB) {
+  return {
+    Home: inning(),
+    Away: inning(),
+  }
 }
+
+
 
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
